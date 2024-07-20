6de3ff5df9ab144fd3e15590c156cd80e8fed263
Lullaby330 <69coder@gmail.com>
2024-07-20 22:59:04 +0700

    Update config.py

--- config.py
+++ config.py
@@ -36,8 +36,8 @@ UPSTREAM_BRANCH = os.environ.get("UPSTREAM_BRANCH", "master")
 DB_URI = os.environ.get("DATABASE_URL", "postgres://piydyfxd:6abFKPXfFbOIFN9fJ6CSz2DBtPdX3iiv@rosie.db.elephantsql.com/piydyfxd")
 
 # ID dari Channel Atau Group Untuk Wajib Subscribenya
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", " -1001313917239"))
-FORCE_SUB_GROUP = int(os.environ.get("FORCE_SUB_GROUP", "0"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001313917239"))
+FORCE_SUB_GROUP = int(os.environ.get("FORCE_SUB_GROUP", "-1001686330541"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
